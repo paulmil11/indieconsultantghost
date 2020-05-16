cde25fd4590cad37701364ee3293388c8ad53351
paulmil11 <46465406+paulmil11@users.noreply.github.com>
2020-05-16 15:23:43 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://styxlab.github.io`,
+    siteUrl: `https://indieconsultant.co`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
