60babbc767bcd87d9cfa3d21a006a835a018a414
paulmil11 <46465406+paulmil11@users.noreply.github.com>
2020-11-06 15:34:53 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,27 +52,6 @@ module.exports = {
         },
         
         
-        {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-HWRT0QK9MF",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: ㄍㄧ,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-      },
-    },
-        
         
         {
             resolve: `gatsby-theme-ghost-dark-mode`,
